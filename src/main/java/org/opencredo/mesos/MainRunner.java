63bc9412f418ee986c1c6e61c0d45682a5a3565d
Bart Spaans <bart.spaans@opencredo.com>
2015-01-20 17:15:03 +0000

    Refactored the MainRunner and got rid off the formatter plugin

--- src/main/java/org/opencredo/mesos/MainRunner.java
+++ src/main/java/org/opencredo/mesos/MainRunner.java
@@ -2,43 +2,60 @@ package org.opencredo.mesos;
 
 import org.apache.mesos.MesosSchedulerDriver;
 import org.apache.mesos.Protos;
+import org.apache.mesos.Protos.FrameworkInfo;
+import org.apache.mesos.Protos.ExecutorInfo;
+import org.apache.mesos.Protos.CommandInfo;
 import org.apache.mesos.Scheduler;
 
 public class MainRunner {
 
-	public static void main(String[] args) throws Exception {
+    static String frameworkName = "framework-example";
+    static String executorName = "PICalculator";
+    static String path = "/vagrant/example-framework-1.0-SNAPSHOT-jar-with-dependencies.jar";
+    static String command = "java -cp /vagrant/example-framework-1.0-SNAPSHOT-jar-with-dependencies.jar org.opencredo.mesos.CalculatePiExecutor";
 
-		Protos.FrameworkInfo.Builder builder = Protos.FrameworkInfo
-				.newBuilder().setHostname("172.16.0.2");
-		builder.setFailoverTimeout(120000).setUser("")
-				.setName("framework-example");
 
-		String path = "/vagrant/example-framework-1.0-SNAPSHOT-jar-with-dependencies.jar";
+    private static FrameworkInfo getFrameworkInfo() {
+        FrameworkInfo.Builder builder = FrameworkInfo.newBuilder();
+        builder.setFailoverTimeout(120000);
+        builder.setUser("");
+        builder.setName(frameworkName);
+        return builder.build();
+    }
 
-		Protos.CommandInfo.URI uri = Protos.CommandInfo.URI.newBuilder()
-				.setValue(path).setExtract(false).build();
+    private static CommandInfo.URI getUri() {
+        CommandInfo.URI.Builder uriBuilder = CommandInfo.URI.newBuilder();
+        uriBuilder.setValue(path);
+        uriBuilder.setExtract(false);
+        return uriBuilder.build();
+    }
 
-		String command = "java -cp /vagrant/example-framework-1.0-SNAPSHOT-jar-with-dependencies.jar org.opencredo.mesos.CalculatePiExecutor";
-		Protos.CommandInfo piCalculatorCommand = Protos.CommandInfo
-				.newBuilder().addUris(uri).setValue(command).build();
+    private static CommandInfo getCommandInfo() {
+        CommandInfo.Builder cmdInfoBuilder = Protos.CommandInfo.newBuilder();
+        cmdInfoBuilder.addUris(getUri());
+        cmdInfoBuilder.setValue(command);
+        return cmdInfoBuilder.build();
+    }
 
-		Protos.ExecutorInfo executorCrawl = Protos.ExecutorInfo
-				.newBuilder()
-				.setExecutorId(
-						Protos.ExecutorID.newBuilder().setValue(
-								"PICalculatorExecutor"))
-				.setCommand(piCalculatorCommand).setName("PICalculator")
-				.setSource("java").build();
+    private static ExecutorInfo getExecutorInfo() {
+        ExecutorInfo.Builder builder = ExecutorInfo.newBuilder();
+        builder.setExecutorId(Protos.ExecutorID.newBuilder().setValue(executorName));
+        builder.setCommand(getCommandInfo());
+        builder.setName(executorName);
+        builder.setSource("java");
+        return builder.build();
+    }
 
-		Scheduler scheduler = new ExampleScheduler(executorCrawl);
-		MesosSchedulerDriver driver = null;
+    private static void runFramework(String mesosMaster) {
+        Scheduler scheduler = new ExampleScheduler(getExecutorInfo());
+        MesosSchedulerDriver driver = new MesosSchedulerDriver(scheduler, getFrameworkInfo(), mesosMaster);
+        int status = driver.run() == Protos.Status.DRIVER_STOPPED ? 0 : 1;
 
-		Protos.FrameworkInfo info = builder.build();
-		System.out.println("Framework hostname: " + info.getHostname());
-		driver = new MesosSchedulerDriver(scheduler, info, args[0]);
-		int status = driver.run() == Protos.Status.DRIVER_STOPPED ? 0 : 1;
+        driver.stop();
+        System.exit(status);
+    }
 
-		driver.stop();
-		System.exit(status);
-	}
+    public static void main(String[] args) throws Exception {
+        runFramework(args[0]);
+    }
 }
